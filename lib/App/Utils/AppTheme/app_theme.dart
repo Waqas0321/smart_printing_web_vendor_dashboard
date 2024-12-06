1f4f793be7195aeb482f605a64dee9b19b9f962d
Waqas Akhtar <waqasakhtar548@gmail.com>
2024-12-07 01:13:33 +0500

    initial commit

--- lib/App/Utils/AppTheme/app_theme.dart
+++ lib/App/Utils/AppTheme/app_theme.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:smart_printing_web/App/Utils/Const/appColors.dart';
 
 class AppTheme {
+
   // Light Theme
   static ThemeData get lightTheme {
     return ThemeData(
--- lib/App/Utils/Const/app_icons.dart
+++ lib/App/Utils/Const/app_icons.dart
@@ -1,3 +1,4 @@
 class AppIcons{
+  static String icon = "iconspAth";
 
 }
