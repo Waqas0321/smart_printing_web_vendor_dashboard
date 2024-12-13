ee387d8f0c89156b39b7be46cea0858b4411e205
Waqas Akhtar <waqasakhtar548@gmail.com>
2024-12-13 15:17:08 +0500

    web firebase connection

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,22 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-
 import 'App/my_app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: FirebaseOptions(
+          apiKey: "AIzaSyCoYBuhKL6QEtej0sEioFKGuMbSNGeQGBY",
+          authDomain: "tellmeapp-f9f0a.firebaseapp.com",
+          projectId: "tellmeapp-f9f0a",
+          storageBucket: "tellmeapp-f9f0a.appspot.com",
+          messagingSenderId: "240195487829",
+          appId: "1:240195487829:web:943695643d62bd18926cdc",
+          measurementId: "G-D4M3K7GXY3"
+      ),
+    );
+  }
   runApp(const MyApp());
 }
